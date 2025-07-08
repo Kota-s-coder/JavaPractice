9bd0f4b082521f57bb20e214260786b74af3dc17
kotasawamoto <kota.sawamoto.engineer@gmail.com>
2025-07-09 04:04:40 +0900

    ch01: サンプルと練習問題回答を追加

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("こんにちは、Java！");
+        int age;
+        age = 30;
+        System.out.println(age);
+
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
+        boolean isError = true;
+        char kanji = '駆';
+        double pie = 3.14;
+        long number = 314159265853979L;
+        String message = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+
+        System.out.println(isError);
+        System.out.println(kanji);
+        System.out.println(pie);
+        System.out.println(number);
+        System.out.println(message);
     }
 }
